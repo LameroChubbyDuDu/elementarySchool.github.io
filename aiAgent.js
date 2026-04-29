1783cf9fac05d192965da711deff1a19534a1be3
LameroChubbyDuDu <proletarian.sad.dudu@gmail.com>
2026-04-29 12:20:09 +0800

    adjusting th ui and prompt

--- aiAgent.js
+++ aiAgent.js
@@ -18,8 +18,9 @@ const openai = new OpenAI({
 app.post('/generate-img', async (req, res) => {
     try {
         const userPrompt = req.body.prompt;
+        console.log("get the prompt", userPrompt);
 
-        const prePrompt = '，像素風格，以32*32為大小去生成。'
+        const prePrompt = '，像素風格，嚴格限制為正前方扁平2D視角(Front flat sprite view)，且必須無立體感(0% depth)，無任何透視(No perspective)，背景必須為純白色，以32*32為大小去生成，不准添加邊框、色票或是其他任何裝飾，且所有物件必須完整顯現在畫面中央，四周需保留一點純白邊距(with wide safe white padding)。'
         const finalPrompt = `${userPrompt}${prePrompt}`
         const resImg = await openai.images.generate({
             model: "dall-e-3",
@@ -31,6 +32,7 @@ app.post('/generate-img', async (req, res) => {
         // const imgUrl = resImg.data[0].url;
         // console.log(imgUrl);
         // res.json({url: imgUrl});
+        
 
         const img = resImg.data[0].b64_json;
         res.json({image: img});
